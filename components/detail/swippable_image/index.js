8e5c452a78d92ffb8df23a069031bd37ec691618
seongmin <berry9901@gmail.com>
2022-09-08 18:25:44 +0900

    fix: image swiper pagination styles

---
+++ components/detail/swippable_image/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+import Swiper from 'react-native-swiper';
+import {colors, height, width} from '../../../utils/globalStyles';
+
+const SwippableImage = ({imgs}) => {
+  return (
+    <Swiper
+      width={375}
+      height={375}
+      removeClippedSubviews={false}
+      dotColor={colors['$gray-7']}
+      dotStyle={styles.dots}
+      activeDotColor={colors.$white}
+      activeDotStyle={styles.dots}
+      paginationStyle={styles.pagination}>
+      {imgs.map(img => (
+        <View key={img}>
+          <Image
+            style={styles.imgs}
+            source={{
+              uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4KbX9ryNVL1pC8iXCLY6lNRAz37Aa7irCYg&usqp=CAU',
+            }}
+          />
+        </View>
+      ))}
+    </Swiper>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {},
+
+  imgs: {
+    width: width * 375,
+    height: height * 375,
+    aspectRatio: 1,
+    resizeMode: 'contain',
+    backgroundColor: colors.$brown,
+  },
+
+  dots: {
+    width: width * 6,
+    height: height * 6,
+  },
+});
+
+export default SwippableImage;
