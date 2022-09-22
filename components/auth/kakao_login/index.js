eb3b8742b6477067022ba88bb1e1b967775f555a
seongmin <berry9901@gmail.com>
2022-09-23 00:35:58 +0900

    feat: 카카오 로그인 UI 만드는 중

--- components/auth/kakao_login/index.js
+++ components/auth/kakao_login/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {Button, Image, Pressable, StyleSheet, Text, View} from 'react-native';
-import {width} from '../../../utils/globalStyles';
+import {
+  Button,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import {colors, fonts, width} from '../../../utils/globalStyles';
 import {KakaoIcon} from '../../svg';
 import Images from './../../../assets/imgs/index';
 
@@ -17,12 +25,18 @@ const KakaoLogin = ({navigation}) => {
         />
       </View>
       <View>
-        <Image />
-        <Pressable>
-          <KakaoIcon />
-          <Text>카카오톡으로 계속하기</Text>
-        </Pressable>
+        <View style={styles.tooltip}>
+          <View style={styles.triangle} />
+          <Text style={styles.tooltipText}>
+            간편하게 로그인하고{'\n'}우리 동네에서 정을 나눠보세요!
+          </Text>
+        </View>
       </View>
+      <Pressable style={styles.kakaoButton}>
+        <KakaoIcon />
+        <Text style={styles.kakaoText}>카카오톡으로 계속하기</Text>
+      </Pressable>
+
       <Pressable>
         <Text>둘러보기</Text>
       </Pressable>
@@ -34,6 +48,50 @@ const styles = StyleSheet.create({
   block: {flex: 1, paddingHorizontal: width * 24, alignItems: 'center'},
 
   img: {marginVertical: width * 52},
+
+  tooltip: {
+    backgroundColor: colors.$white,
+    paddingVertical: width * 12,
+    paddingHorizontal: width * 20,
+    elevation: 5,
+    borderRadius: width * 10,
+    marginBottom: width * 25,
+    position: 'relative',
+  },
+
+  triangle: {
+    width: width * 10,
+    height: width * 10,
+    position: 'absolute',
+    bottom: -width * 10,
+    left: '50%',
+    transform: [{translateX: width * 10}],
+    borderLeftWidth: width * 10,
+    borderLeftColor: 'transparent',
+    borderRightWidth: width * 10,
+    borderRightColor: 'transparent',
+    borderTopWidth: width * 10,
+    borderTopColor: colors.$white,
+  },
+
+  tooltipText: {textAlign: 'center', ...fonts.H6, color: colors['$gray-4']},
+
+  kakaoButton: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5E14B',
+    paddingVertical: width * 10,
+    borderRadius: width * 10,
+  },
+
+  kakaoText: {
+    fontFamily: 'AppleSDGothicNeoM',
+    fontSize: width * 16,
+    letterSpacing: width * -0.53,
+    color: colors['$gray-1'],
+  },
 });
 
 export default KakaoLogin;
