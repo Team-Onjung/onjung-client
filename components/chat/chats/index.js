03408e69f17408d8554ad8a4c7af8dfda3311d54
zizonyoungjun <naya700@naver.com>
2022-12-25 22:12:53 +0900

    fix: 채팅리스트 ui 수정

--- components/chat/chats/index.js
+++ components/chat/chats/index.js
@@ -26,6 +26,15 @@ const Chats = () => {
   const [value, setValue] = useState('대여 가능');
   return (
     <SafeAreaView>
+      <View style={styles.settings}>
+        <AlarmIcon
+          onPress={() => {
+            navigation.navigate('Search');
+          }}
+          style={styles.alarm}
+        />
+      </View>
+      <View style={styles.borderLine} />
       <ScrollView style={styles.chatContainer}>
         {tip.map((content, i) => (
           <View style={styles.chatList} key={i}>
@@ -36,14 +45,21 @@ const Chats = () => {
               style={styles.total}>
               <Image
                 style={styles.profileImage}
-                source={require('../../../assets/imgs/main/Rectangle.png')}
+                source={require('../../../assets/imgs/main/blank-profile.png')}
               />
               <View style={styles.chat}>
                 <Text style={styles.userName} numberOfLines={1}>
                   {content.user_name}
                 </Text>
-                <Text style={styles.chatContents}>{content.lastChat}</Text>
-                <Text>{content.time}분 전</Text>
+                <View flexDirection="row">
+                  <Text
+                    style={styles.chatContents}
+                    numberOfLines={1}
+                    ellipsizeMode="tail">
+                    {content.lastChat}
+                  </Text>
+                  <Text style={styles.chatContents}>• {content.time}분 전</Text>
+                </View>
               </View>
             </Pressable>
           </View>
@@ -54,23 +70,30 @@ const Chats = () => {
 };
 
 const styles = StyleSheet.create({
+  settings: {
+    flexDirection: 'row',
+    marginTop: width * 26,
+    marginLeft: width * 330,
+    marginBottom: width * 6,
+  },
+
   chatContainer: {
     height: height * 600,
-    marginTop: width * 50, //배너 넣으면 어떻까해서 만들어본 공백
   },
 
   chatList: {
     flex: 1,
     flexDirection: 'row',
+    marginBottom: width * 5,
   },
   total: {
     flexDirection: 'row',
   },
   profileImage: {
     flexDirection: 'row',
-    width: width * 60,
-    height: height * 65,
-    borderRadius: 20,
+    width: width * 46,
+    height: height * 56,
+    borderRadius: 100,
     marginTop: width * 15,
     marginLeft: width * 25,
     marginRight: width * 5,
@@ -88,10 +111,20 @@ const styles = StyleSheet.create({
   },
   chatContents: {
     fontFamily: 'Apple SD Gothic Neo',
-    fontSize: width * 15,
+    fontSize: width * 12,
     color: colors['$gray-5'],
     marginBottom: width * 5,
-    marginRight: width * 120,
+    marginRight: width * 10,
+    width: '45%',
+  },
+  borderLine: {
+    height: 1,
+    width: 360,
+    backgroundColor: colors['$gray-7'],
+    marginTop: width * 9,
+    marginBottom: width * 9,
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
 });
 
